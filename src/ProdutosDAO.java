458d873e24e8f58f3ea22641cbd949b5c649c201
Poukk <alexandrefilho.06@hotmail.com>
2025-06-30 20:06:12 -0300

    refactor: remove useless auto-generated comments

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,15 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
-import java.sql.PreparedStatement;
-import java.sql.Connection;
+import java.sql.PreparedStatement;import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -23,20 +12,11 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
         //conn = new conectaDAO().connectDB();
-        
-        
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
         return listagem;
     }
-    
-    
-    
-        
 }
 
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,27 +1,12 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-public class conectaDAO {
-    
+public class conectaDAO {    
     public Connection connectDB(){
         Connection conn = null;
-        
         try {
-        
             conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
             
         } catch (SQLException erro){
@@ -29,5 +14,4 @@ public class conectaDAO {
         }
         return conn;
     }
-    
 }
